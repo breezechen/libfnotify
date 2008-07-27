77b112787f95ed7c848a69c82a40d4787d010780
vlovich <vlovich@554c396b-ab4a-0410-b1af-aff0ce8c8d3c>
2008-07-27 05:17:46 +0000

    Accidentally doing cleanup before entering the directory where the test is run.

--- trunk/testsuite.sh
+++ trunk/testsuite.sh
@@ -8,9 +8,9 @@ function cleanup_old_tests()
 TESTS="plugin_test core_test smoke_test functionality_test"
 BIN_DIR=bin
 
-cleanup_old_tests
-
 cd "$BIN_DIR"
+
+cleanup_old_tests
 
 export LD_LIBRARY_PATH=.
 
