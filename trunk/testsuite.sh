8fafeb66d16f1376b47a8b98430bc70ce2df395a
vlovich <vlovich@554c396b-ab4a-0410-b1af-aff0ce8c8d3c>
2008-07-27 05:25:05 +0000

    Compile with optimal number of threads before running testsuite.

--- trunk/testsuite.sh
+++ trunk/testsuite.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+# Die with some kind of message
+function die()
+{
+	echo $@ >&2
+	exit 1
+}
+
+# Remove old data that may interfere with running the tests
 function cleanup_old_tests()
 {
+	# Cleanup for functionality_test
 	rm -r step_*.tmp 2> /dev/null
 }
 
 TESTS="plugin_test core_test smoke_test functionality_test"
 BIN_DIR=bin
+
+case $(uname) in
+	"Linux")
+		NUM_CPUS=$(cat /proc/cpuinfo | grep '^processor	: [0-9][0-9]*$' | wc -l)
+		;;
+	*)
+		NUM_CPUS=1
+		;;
+esac
+
+NUM_THREADS=$((NUM_CPUS+1))
+
+echo "Compiling with $NUM_THREADS threads"
+make -j$NUM_THREADS >/dev/null 2>&1 || die "Failed to compile project"
 
 cd "$BIN_DIR"
 
