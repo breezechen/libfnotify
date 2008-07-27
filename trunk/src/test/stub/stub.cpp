22e0905bf8ee10ed17b82ea8436ab5a24fc47d44
vlovich <vlovich@554c396b-ab4a-0410-b1af-aff0ce8c8d3c>
2008-07-27 04:43:59 +0000

    Stub test - makes sure linking is correct.

---
+++ trunk/src/test/stub/stub.cpp
@@ -0,0 +1,16 @@
+//
+// C++ Implementation: stub
+//
+// Description: 
+//
+//
+// Author: Vitali Lovich <vlovich@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+int main()
+{
+	return 0;
+}
